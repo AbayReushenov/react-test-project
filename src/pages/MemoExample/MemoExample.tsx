d7879e2e4aabf351ce7652ce08889b1713a89ce4
AbayReushenov <abay.reushenov@gmail.com>
2025-11-10 16:01:44 +0300

    Fix stile

--- src/pages/MemoExample/MemoExample.tsx
+++ src/pages/MemoExample/MemoExample.tsx
@@ -1,22 +1,23 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react'
 
 function MemoExample() {
-    const [count, setCount] = useState(0);
-     const [todos, setTodos] = useState<string[]>([]); // Явный тип: массив строк
+    const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState<string[]>([]) // Явный тип: массив строк
 
     // Симуляция дорогого вычисления: сумма от 1 до n с циклом
     const expensiveValue = useMemo(() => {
-        console.log('Вычисление выполнено'); // Лог для демонстрации: выводится только при изменении count
-        let sum = 0;
-        for (let i = 0; i <= count * 1000; i++) { // Имитация тяжелой операции
-            sum += i;
+        console.log('Вычисление выполнено') // Лог для демонстрации: выводится только при изменении count
+        let sum = 0
+        for (let i = 0; i <= count * 1000; i++) {
+            // Имитация тяжелой операции
+            sum += i
         }
-        return sum;
-    }, [count]); // Зависимость только от count
+        return sum
+    }, [count]) // Зависимость только от count
 
     const addTodo = () => {
-        setTodos([...todos, `Todo ${todos.length + 1}`]);
-    };
+        setTodos([...todos, `Todo ${todos.length + 1}`])
+    }
 
     return (
         <div>
@@ -25,9 +26,13 @@ function MemoExample() {
             <p>Смотрите логи! Лог для демонстрации: выводится только при изменении count</p>
             <button onClick={() => setCount(count + 1)}>Увеличить count ({count})</button>
             <button onClick={addTodo}>Добавить todo ({todos.length})</button>
-            <ul>{todos.map((todo, index) => <li key={index}>{todo}</li>)}</ul>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
         </div>
-    );
+    )
 }
 
-export default MemoExample;
+export default MemoExample
